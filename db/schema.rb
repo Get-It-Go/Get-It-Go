6c1ff6eda08c28d2891848a94ded6665dc046e02
changgwon <rnjs9409@naver.com>
2018-08-25 03:16:38 +0900

    edit layout

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,21 @@
 
 ActiveRecord::Schema.define(version: 20180824175508) do
 
+  create_table "products", force: :cascade do |t|
+    t.text "name"
+    t.text "content"
+    t.boolean "status", default: false
+    t.text "image"
+    t.text "price"
+    t.text "address_num"
+    t.text "address_text"
+    t.text "address_text2"
+    t.integer "user_id"
+    t.text "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -30,21 +45,6 @@ ActiveRecord::Schema.define(version: 20180824175508) do
     t.string "profile_img"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-ActiveRecord::Schema.define(version: 20180824152902) do
-
-  create_table "products", force: :cascade do |t|
-    t.text "name"
-    t.text "content"
-    t.boolean "status", default: false
-    t.text "image"
-    t.text "price"
-    t.text "address_num"
-    t.text "address_text"
-    t.text "address_text2"
-    t.integer "user_id"
-    t.text "category"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
