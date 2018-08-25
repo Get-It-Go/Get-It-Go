34457f9572dd11544bdfd4d62ad72cc79ab7ea45
limdongjin <30315743+limdongjin@users.noreply.github.com>
2018-08-26 03:37:05 +0900

    fuckin error resolve (#20)

--- config/routes.rb
+++ config/routes.rb
@@ -9,10 +9,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/products/:id/complete" => "products#complete"
   get "/home" => "home#index"
-<<<<<<< HEAD
   get '/home/main' => "home#main"
-=======
-  
-  
->>>>>>> ccb70ef808ad40b40f52b0035847e5a4364dca38
-end
+end 
