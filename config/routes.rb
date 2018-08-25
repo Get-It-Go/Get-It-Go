736416142b11da58d3ceee1c5384fb3f7ab58994
chosunme96 <31688874+chosunme96@users.noreply.github.com>
2018-08-26 03:01:12 +0900

    before front

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/products/:id/complete" => "products#complete"
   get "/home" => "home#index"
+  get '/home/main' => "home#main"
 end
